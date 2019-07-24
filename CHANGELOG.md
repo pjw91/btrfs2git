f82b8aed0563c9f5910a7c1fdfd18ee484d73d41
Patrick Wang <mail6543210@yahoo.com.tw>
2019-07-24 23:34:39 +0800

    Operate on git index instead of worktree

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ This project adheres to [Semantic Versioning](http://semver.org/) and [Keep a Ch
 ### New
 
 ### Changes
+* Operate on index instead of worktree
+  * Speedup: 6x ~ 12x
 
 ### Fixes
 
--- btrfs2git.py
+++ btrfs2git.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 import argparse
+import functools
 import logging
 import os
 import re
 import shutil
 from subprocess import check_call, check_output, DEVNULL, PIPE, run
+import tempfile
 
 os.putenv('GIT_AUTHOR_DATE', 'unix:100000000')
 os.putenv('GIT_COMMITTER_DATE', 'unix:100000000')
 WB = re.compile(r'^Well block (\d+)\(gen: (\d+) level: ')
 RT = re.compile(r'^Found tree root at (\d+) gen (\d+) level ')
+
+
+@functools.lru_cache(typed=True)
+def write_object(b):
+    return check_output(['git', 'hash-object', '-w', '--stdin'], input=b, cwd=args.path).strip()
 
 
 def exclude(path):
@@ -24,6 +31,7 @@ def handle_restore(proc, m):
     _p = re.escape(args.path)
     REGULAR = re.compile(r'^Restoring (%s/.+)$' % _p)
     SYMLINK = re.compile(r"^SYMLINK: '(%s/.+)' => '.*'$" % _p)
+    info, dirs = [], set()
     for ff in reversed(proc.stdout.decode().splitlines()):
         try:
             if ff.startswith('Restoring '):
@@ -35,13 +43,21 @@ def handle_restore(proc, m):
             path = F.match(ff).group(1)
             if exclude(path):
                 continue
-            os.makedirs(os.path.dirname(path), 0o777, exist_ok=True)
-            if not os.path.isdir(path):
-                open(path, 'w').write(ff[0])
+            path = path[len(args.path)+1:]
+            dirs.add(os.path.dirname(path))
+            if path not in dirs:
+                info.append(b'%s %s\t%s' % (b'100644', write_object(ff[0].encode('ascii')), path.encode()))
         except Exception:
             logging.exception('Exception happens when processing "%s"', ff)
             raise
-    check_call(['git', 'add', '-Af', '.'], cwd=args.path, stdout=DEVNULL)
+    new_idx = tempfile.mkstemp()[1]
+    os.unlink(new_idx)
+    os.putenv('GIT_INDEX_FILE', new_idx)
+    check_call(['git', 'update-index', '-z', '--remove', '--index-info'],
+               input=b'\0'.join(info), cwd=args.path, stderr=DEVNULL)
+    if os.path.isfile(new_idx):
+        shutil.move(new_idx, os.path.join(args.path, '.git', 'index'))
+        os.unsetenv('GIT_INDEX_FILE')
     check_call(['git', 'commit', '--allow-empty', '-m', m], cwd=args.path, stdout=DEVNULL)
 
 
